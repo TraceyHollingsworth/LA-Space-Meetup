cdc3ade0bfafd5dbc84f57c8e9559d38246113b8
TraceyHollingsworth <tracey.hollingsworth@gmail.com>
2014-12-09 15:24:45 -0500

    add colums to meetups and remove location_id from meetups

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141209192417) do
+ActiveRecord::Schema.define(version: 20141209202214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,11 +19,15 @@ ActiveRecord::Schema.define(version: 20141209192417) do
   create_table "meetups", force: true do |t|
     t.string   "title",       null: false
     t.text     "description", null: false
-    t.integer  "location_id", null: false
     t.datetime "date",        null: false
     t.datetime "time",        null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "location",    null: false
+    t.string   "address",     null: false
+    t.string   "city",        null: false
+    t.string   "state",       null: false
+    t.string   "zipcode",     null: false
   end
 
   create_table "rsvps", force: true do |t|
